2fce30fa40aca0e17f92fb0e5eb2fdc4b248bf8b
Jaideep Chunduri <gn8681@wayne.edu>
2023-02-07 14:18:02 -0500

    Profile Page functionality

--- food_for_thought/lib/profile_page.dart
+++ food_for_thought/lib/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -8,8 +9,26 @@ class ProfilePage extends StatefulWidget {
   @override
   ProfilePageState createState() => ProfilePageState();
 }
-
 class ProfilePageState extends State<ProfilePage> {
+  final user = FirebaseAuth.instance.currentUser!;
+
+
+//document ID's
+  List<String> docIDs = [];
+
+
+// get docIDs
+  Future getDocId() async {
+    await FirebaseFirestore.instance
+        .collection('user')
+        .get()
+        .then((snapshot) => snapshot.docs.forEach((document) {
+              print(document.reference);
+              docIDs.add(document.reference.id);
+            }));
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +37,39 @@ class ProfilePageState extends State<ProfilePage> {
         backgroundColor: Colors.red,
         automaticallyImplyLeading: false,
       ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'signed in as:  ${user?.email!}',
+              style: TextStyle(fontSize: 20),
+            ),
+            MaterialButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                color: Colors.red[200],
+                child: Text('sign out')),
+            Expanded(
+              child: FutureBuilder(
+                future: getDocId(),
+                builder: (context, snapshot) {
+                  return ListView.builder(
+                    itemCount: docIDs.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(docIDs[index]),
+                      );
+                    },
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
+
+
