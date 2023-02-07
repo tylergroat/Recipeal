00c6ded3da364b4aa90012e81fdf83debd60c560
Jaideep Chunduri <gn8681@wayne.edu>
2023-02-07 15:45:16 -0500

    Profile Page functionality (displays full name and user name of all users in database)

--- food_for_thought/lib/profile_page.dart
+++ food_for_thought/lib/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:food_for_thought/read%20data/get_user_name.dart';
 
 import 'authentification.dart';
 import 'login_page.dart';
@@ -9,25 +10,23 @@ class ProfilePage extends StatefulWidget {
   @override
   ProfilePageState createState() => ProfilePageState();
 }
+
 class ProfilePageState extends State<ProfilePage> {
   final user = FirebaseAuth.instance.currentUser!;
-
 
 //document ID's
   List<String> docIDs = [];
 
-
 // get docIDs
   Future getDocId() async {
     await FirebaseFirestore.instance
-        .collection('user')
+        .collection('users')
         .get()
         .then((snapshot) => snapshot.docs.forEach((document) {
               print(document.reference);
               docIDs.add(document.reference.id);
             }));
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -37,39 +36,39 @@ class ProfilePageState extends State<ProfilePage> {
         backgroundColor: Colors.red,
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              'signed in as:  ${user?.email!}',
-              style: TextStyle(fontSize: 20),
-            ),
-            MaterialButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                color: Colors.red[200],
-                child: Text('sign out')),
-            Expanded(
-              child: FutureBuilder(
-                future: getDocId(),
-                builder: (context, snapshot) {
-                  return ListView.builder(
-                    itemCount: docIDs.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(docIDs[index]),
-                      );
-                    },
-                  );
-                },
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                'Signed in as:  ${user?.email!}',
+                style: TextStyle(fontSize: 20),
               ),
-            )
-          ],
+      
+              Expanded(
+                child: FutureBuilder(
+                  future: getDocId(),
+                  builder: (context, snapshot) {
+                    return ListView.builder(
+                      itemCount: docIDs.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            title: GetUserName(documentID: docIDs[index]),
+                            tileColor: Colors.grey[200],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
